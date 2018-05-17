37a46623eb63eb962bb2930150bdc83a4ce7647c
Fendo181 <fendo181git@gmail.com>
2018-05-18 08:27:36 +0900

    Home画面の関すvueコンポーネントを差ky生した。

---
+++ mymarkdown/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="home">
+        <h1>{{ msg}}</h1>
+        <button>Googleアカウントでログイン</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data() {
+        return{
+            msg: "Welcome to MyMarkdown"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
